9f6e3a0615363a833bf0e1445a9aa35838956254
touhou09 <touhou3178@gmail.com>
2024-04-02 15:21:56 +0900

    spring setup

---
+++ serviceServer/transfer/src/test/java/style_transfer/transfer/TransferApplicationTests.java
@@ -0,0 +1,13 @@
+package style_transfer.transfer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
