7c2157c4a3429d0f0c0db7280c563ec2a29c1f41
touhou09 <touhou3178@gmail.com>
2024-05-13 17:15:31 +0900

    user dto and sign up controller edit

--- serviceServer/transfer/src/main/java/style_transfer/transfer/controller/UserController.java
+++ serviceServer/transfer/src/main/java/style_transfer/transfer/controller/UserController.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import style_transfer.transfer.repository.User;
 import style_transfer.transfer.repository.UserRepository;
+import style_transfer.transfer.repository.generatedImageResponseDto;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 
@@ -21,11 +25,13 @@ public class UserController {
         String email = principal.getAttribute("email");
         if (userRepository.findByEmail(email) == null) {
             String name = principal.getAttribute("name");
-            User user = new User(null, email, name);
+            List<generatedImageResponseDto> initialProjects = new ArrayList<>();  // 초기 프로젝트 목록 생성
+            User user = new User(null, email, name, initialProjects);  // 프로젝트 목록 포함하여 사용자 객체 생성
             userRepository.save(user);
         }
         return "redirect:/user-profile";
     }
+
 
     @GetMapping("/user-profile")
     public ResponseEntity<User> userProfile(@AuthenticationPrincipal OAuth2User principal) {
--- serviceServer/transfer/src/main/java/style_transfer/transfer/repository/User.java
+++ serviceServer/transfer/src/main/java/style_transfer/transfer/repository/User.java
@@ -6,6 +6,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
@@ -15,4 +17,5 @@ public class User {
     private String id;
     private String email;
     private String name;
+    private List<generatedImageResponseDto> Projects;
 }
