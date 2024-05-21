1b0f8e24e638b6af6c984f4b9e11f05259f10bc5
touhou09 <touhou3178@gmail.com>
2024-05-21 12:55:19 +0900

    db save

--- serviceServer/transfer/src/main/java/style_transfer/transfer/repository/UserRepository.java
+++ serviceServer/transfer/src/main/java/style_transfer/transfer/repository/UserRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
     User findByEmail(String email);
+    User findByToken(String token);
 }
--- serviceServer/transfer/src/main/java/style_transfer/transfer/service/ImageGenerationService.java
+++ serviceServer/transfer/src/main/java/style_transfer/transfer/service/ImageGenerationService.java
@@ -5,6 +5,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import style_transfer.transfer.repository.User;
+import style_transfer.transfer.repository.UserRepository;
 import style_transfer.transfer.repository.generatedImageResponseDto;
 import style_transfer.transfer.repository.promptRequestDto;
 
@@ -12,10 +14,12 @@ import style_transfer.transfer.repository.promptRequestDto;
 public class ImageGenerationService {
 
     private final WebClient webClient;
+    private final UserRepository userRepository;
 
     @Autowired
-    public ImageGenerationService(WebClient.Builder webClientBuilder) {
+    public ImageGenerationService(WebClient.Builder webClientBuilder, UserRepository userRepository) {
         this.webClient = webClientBuilder.baseUrl("http://localhost:8000").build();
+        this.userRepository = userRepository;
     }
 
     public Mono<generatedImageResponseDto> generateImages(promptRequestDto request) {
@@ -25,6 +29,15 @@ public class ImageGenerationService {
                 .body(Mono.just(request), promptRequestDto.class) // 직접 받은 promptRequestDto를 사용
                 .retrieve()
                 .bodyToMono(generatedImageResponseDto.class)
+                .flatMap(response -> {
+                    // 응답을 DB에 저장
+                    User user = userRepository.findByToken(request.getToken());
+                    if (user != null) {
+                        user.getProjects().add(response);
+                        userRepository.save(user);
+                    }
+                    return Mono.just(response);
+                })
                 .onErrorMap(WebClientResponseException.class, ex -> new Exception("API call failed: " + ex.getMessage()));
     }
 }
