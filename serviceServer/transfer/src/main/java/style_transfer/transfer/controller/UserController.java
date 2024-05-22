0b217e86b0544be76b963973d04dc07de36a17bf
touhou09 <touhou3178@gmail.com>
2024-05-22 16:53:48 +0900

    get user projects

--- serviceServer/transfer/src/main/java/style_transfer/transfer/controller/UserController.java
+++ serviceServer/transfer/src/main/java/style_transfer/transfer/controller/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.security.web.bind.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
 import style_transfer.transfer.repository.User;
 import style_transfer.transfer.repository.UserRepository;
@@ -16,7 +17,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Controller
-
+@RequestMapping("/api/user")
 public class UserController {
 
     @Autowired
@@ -46,6 +47,19 @@ public class UserController {
         User user = userService.getUserByToken(token);
         if (user != null) {
             return ResponseEntity.ok(user);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @GetMapping("/projects")
+    public ResponseEntity<List<generatedImageResponseDto>> userProjects(@RequestHeader("Authorization") String token) {
+        if (token.startsWith("Bearer ")) {
+            token = token.substring(7); // "Bearer " 부분을 제거
+        }
+        User user = userService.getUserByToken(token);
+        if (user != null) {
+            return ResponseEntity.ok(user.getProjects());
         } else {
             return ResponseEntity.notFound().build();
         }
