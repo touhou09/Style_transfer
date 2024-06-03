b9a0a947a895585e084e31d809de976621f12ec4
msm11 <128119495+msm11@users.noreply.github.com>
2024-06-03 19:13:34 +0900

    Update main.py

--- modelServer/app/main.py
+++ modelServer/app/main.py
@@ -1,9 +1,6 @@
 from fastapi import FastAPI
-from routers import items, example_images, generate_images
+from routers import example_images, generate_images
 
 app = FastAPI()
-# app = FastAPI()
-
-app.include_router(items.router, prefix="/items")
 app.include_router(generate_images.router)
 app.include_router(example_images.router)
